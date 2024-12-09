342c8450e696bf417b801cdf6cfa93ba0aa9adff
suhishan <suhishanbhandari@gmail.com>
2024-12-09 20:34:37 +0545

    Created a payoff function

--- political_agency_model.R
+++ political_agency_model.R
@@ -23,15 +23,38 @@ r_1 <- rnorm(N, mean = 40, sd = 10) # for now its normal, we'll think about bett
 
 #Candidate pool :1 is for congruent and 2 is for dissonants.
 cand_pool <- rbinom(N,1, prob = 0.5) +1
-# Policy Action is a function of s and cand_pool
 
+# Policy Action is a function of s and cand_pool
 payoff <- vector(length = N) # delta here is 1 and no payoff is 0
 payoff <- ifelse(cand_pool == 2, ifelse(r_1 > beta*(40 + E),0,1), 1)
 
-# empirical delta is the probability that the dissonant politicians acted as congruent
+# Empirical delta is the probability that the dissonant politicians acted as congruent
 # given the insufficiency of rent in period 1.
 emp_delta <- 1 - (sum(payoff == 0)/sum(cand_pool == 2))
-#Then by bayes rule, the new pi, i.e. perceived pi of voters after seeing the
+
+# Then by bayes rule, the new pi, i.e. perceived pi of voters after seeing the
 # payoff is:
 
 big_pi <- pi/ (pi + (1-pi)*emp_delta)
+
+
+
+# Let's make a function that does this for us -----------------------------
+
+payoff_func <- function(mu, N, beta, pi, E) {
+  s <- sample(c(0,1), N, replace = TRUE) # randomized states of the world.
+  r_1 <- rnorm(N[cand_pool == 2], mean = mu, sd = 10) #realized rents in period 1.
+  cand_pool <- rbinom(N,1, prob = 0.5) +1 # candidate pool. 1 for congruent and 2 for dissonants
+  
+  payoff <- vector(length = N) # delta here is 1 and no payoff is 0
+  payoff <- ifelse(cand_pool == 2, ifelse(r_1 < beta*(mu + E),1,0), 1)
+  
+  emp_delta <- 1 - (sum(payoff == 0)/sum(cand_pool == 2))
+  big_pi <- pi/ (pi + (1-pi)*emp_delta)
+  
+  return (list(empirical_delta = emp_delta,
+              pi = pi,
+              big_pi = big_pi))
+}
+
+a <- payoff_func(mu = 30, N = 1e4, beta = 0.9, pi = 0.5, E = 10)
